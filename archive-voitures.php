040e4e7fd192b5b9ad9aab627918aaa3702a08de
Déborah Gluck <deborah.gluck@icloud.com>
2020-10-01 09:40:05 +0200

    customisation page archive voitures

--- archive-voitures.php
+++ archive-voitures.php
@@ -3,15 +3,12 @@
 <?php if (have_posts()) : ?>
 <?php while (have_posts()) : the_post(); ?>
 <article class="projet">
-<?php the_post_thumbnail( 'thumbnail' ); ?>
 <h1 class="title">
 <a href="<?php the_permalink(); ?>">
 <?php the_title(); ?>
 </a>
 </h1>
-<div class="content">
-<?php the_content(); ?>
-</div>
+<?php the_post_thumbnail( 'thumbnail' ); ?>
 
 <!-- Ajout taxonomy -->
 <div>
