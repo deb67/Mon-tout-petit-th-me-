168b3656553093359f526f74b3f453a3d663efbe
Déborah Gluck <deborah.gluck@icloud.com>
2020-10-01 09:56:13 +0200

    Affichage de mes caractéristiques sur une page single

--- single-voitures.php
+++ single-voitures.php
@@ -8,6 +8,15 @@
 </h1>
 <div class="content">
 <?php the_content(); ?>
+<!-- Ajout taxonomy -->
+<div>
+<?php the_terms( $post->ID, 'dimensions', 'Dimensions : ' ); ?><br>
+<?php the_terms( $post->ID, 'couleur', 'Couleur : ' ); ?><br>
+<?php the_terms( $post->ID, 'cylindre', 'Cylindré : ' ); ?><br>
+<?php the_terms( $post->ID, 'annee', 'Année : ' ); ?><br>
+<?php the_terms( $post->ID, 'modele', 'Modèle : ' ); ?><br>
+<?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br>
+</div> 
 </div>
 </article>
 <?php endwhile; ?>
