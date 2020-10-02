3b4a3fcb2c18cda2a6fa7038188bdc3641be0c49
Déborah Gluck <deborah.gluck@icloud.com>
2020-10-02 08:33:20 +0200

    création page taxonomy-marque
    permet d'afficher les voitures d'une marque en particulier

--- page.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$args = array(
-    'post_type' => 'voitures', // type de publication
-    'meta_key' => 'cylindres', // C'est ici qu'on indique quel est ce champ
-    'meta_value_num' => 20, // ou meta_value pour tester un texte
-    'meta_compare' => '>' // < > != >= <=
-);
-$query = new WP_Query( $args );
