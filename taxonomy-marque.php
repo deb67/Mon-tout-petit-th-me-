4fb4e80320c7e2fef8828b3026452c36d5331052
Déborah Gluck <deborah.gluck@icloud.com>
2020-10-02 08:50:07 +0200

    test 2 taxonomy

--- taxonomy-marque.php
+++ taxonomy-marque.php
@@ -1,23 +1,28 @@
 <?php get_header(); ?>
 <div class="projets">
-  <p class="heading">
-    Type : <?php single_term_title(); ?>
-  </p>
-  <?php if (have_posts()) : ?>
+    <p class="heading">
+        Type : <?php single_term_title(); ?>
+    </p>
+    <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
-      <article class="projet">
+    <article class="projet">
+        <?php the_terms( $post->ID, 'dimensions', 'Dimensions : ' ); ?><br>
+        <?php the_terms( $post->ID, 'couleur', 'Couleur : ' ); ?><br>
+        <?php the_terms( $post->ID, 'cylindre', 'Cylindré : ' ); ?><br>
+        <?php the_terms( $post->ID, 'annee', 'Année : ' ); ?><br>
+        <?php the_terms( $post->ID, 'modele', 'Modèle : ' ); ?><br>
         <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br>
         <?php the_post_thumbnail( 'thumbnail' ); ?>
         <h1 class="title">
-          <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-          </a>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
         </h1>
         <div class="content">
-          <?php the_content(); ?>
+            <?php the_content(); ?>
         </div>
-      </article>
+    </article>
     <?php endwhile; ?>
-  <?php endif; ?>
+    <?php endif; ?>
 </div>
 <?php get_footer(); ?>
