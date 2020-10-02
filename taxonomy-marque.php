ae187f197a530ffd29efd5c648ee668d5b78a001
Déborah Gluck <deborah.gluck@icloud.com>
2020-10-02 08:51:18 +0200

    test

--- taxonomy-marque.php
+++ taxonomy-marque.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="projets">
     <p class="heading">
-        Type : <?php single_term_title(); ?>
+        Marque : <?php single_term_title(); ?>
     </p>
     <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
