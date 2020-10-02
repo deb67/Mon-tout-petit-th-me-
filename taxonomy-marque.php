5790ad9b3854ccdec9215714a2b51c0aafea7e0e
Déborah Gluck <deborah.gluck@icloud.com>
2020-10-02 09:27:41 +0200

    taxonomy marque ok

--- taxonomy-marque.php
+++ taxonomy-marque.php
@@ -6,12 +6,6 @@
     <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
     <article class="projet">
-        <?php the_terms( $post->ID, 'dimensions', 'Dimensions : ' ); ?><br>
-        <?php the_terms( $post->ID, 'couleur', 'Couleur : ' ); ?><br>
-        <?php the_terms( $post->ID, 'cylindre', 'Cylindré : ' ); ?><br>
-        <?php the_terms( $post->ID, 'annee', 'Année : ' ); ?><br>
-        <?php the_terms( $post->ID, 'modele', 'Modèle : ' ); ?><br>
-        <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br>
         <?php the_post_thumbnail( 'thumbnail' ); ?>
         <h1 class="title">
             <a href="<?php the_permalink(); ?>">
